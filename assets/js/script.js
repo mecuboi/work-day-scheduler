128441877bdf7eb6933c4ba3440da6aaed40c11d
mecuboi <matthewd.jusuf@gmail.com>
2022-08-27 15:49:25 +1000

    adding table to html via JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
 var currentDay = $('#currentDay');
-
+var container = $('.container')
+var tableEl = $('<table>') 
+var thEl = $('<th>')
+var trEl = $('<tr>')
+var tdEl = $('<td>')
+var table = $('.table')
+var row = $('.row')
+var hour = $('.hour')
+var timeBlock = $('.time-block')
+//showing the current day in the header
 currentDay.text(moment().format('dddd, MMMM Do'));
 
+//creating the blocks in the scheduler
+container.append(tableEl.addClass('table'));
+table.append(trEl.addClass('row'))
+row.append(thEl.addClass('hour'))
+row.append(tdEl.addClass('time-block'))
 
 
 

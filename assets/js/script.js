6a4171b727ac33fd80ae5495b9a83f000448b586
mecuboi <matthewd.jusuf@gmail.com>
2022-08-27 16:43:02 +1000

    fixing save

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,20 @@ var container = $('.container')
 currentDay.text(moment().format('dddd, MMMM Do'));
 
 //creating the blocks in the scheduler
-// container.append($('<div>').addClass('time-block'));
-// var timeBlock = $('.time-block')
+container.append($('<div>').addClass('time-block'));
+var timeBlock = $('.time-block')
 
-// table.append($('<div>').addClass('row'));
-// var row = $('.row')
+timeBlock.append($('<div>').addClass('row'));
+var row = $('.row')
 
-// row.append($('<span>').addClass('hour'));
-// var hour = $('.hour')
+row.append($('<span>').addClass('hour'));
+var hour = $('.hour')
 
-// row.append($('<textarea>').addClass('description'));
-// var description = $('.description')
+row.append($('<textarea>').addClass('description'));
+var description = $('.description')
 
-// row.append($('<span>').addClass('saveBtn'));
-// var saveBtn = $('.saveBtn');
+row.append($('<span>').addClass('saveBtn'));
+var saveBtn = $('.saveBtn');
 
 
 

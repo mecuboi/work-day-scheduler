732730f55502e2f84d3fdaf51ae091411e1ed63f
mecuboi <matthewd.jusuf@gmail.com>
2022-09-01 11:21:53 +1000

    fixing html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,51 @@
 var currentDay = $('#currentDay');
 var container = $('.container')
-
-
+var workingHours = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM']
 
 //showing the current day in the header
 currentDay.text(moment().format('dddd, MMMM Do'));
 
-//creating the blocks in the scheduler
-container.append($('<div>').addClass('time-block'));
+container
+    .append($('<div>')
+    .addClass('time-block'));
 var timeBlock = $('.time-block')
 
-timeBlock.append($('<div>').addClass('row'));
+//creating the rows in the scheduler
+timeBlock
+    .append($('<div>')
+    .addClass('row'));
 var row = $('.row')
 
-row.append($('<span>').addClass('hour'));
+row
+    .append($('<span>')
+    .addClass('hour'));
 var hour = $('.hour')
 
-row.append($('<textarea>').addClass('description'));
+row
+    .append($('<textarea>')
+    .addClass('description'));
 var description = $('.description')
 
-row.append($('<button>').addClass('saveBtn'));
+row
+    .append($('<button>')
+    .addClass('saveBtn'));
 var saveBtn = $('.saveBtn');
 
-saveBtn.append($('<i>'));
+saveBtn
+    .append($('<i>'));
 var saveIcon = $('i');
 
-hour.text('9AM')
+hour.text('9 AM')
 saveIcon.text('💾')
+
+//looping over the rows
+for (var i = 0; i < workingHours.length; i++)
+    
+
+
+
+
+
 
 
 
